bb322791ccc090cb2e9ace3ab11a3af195563b75
Sharon Sung <sungsharon@gmail.com>
2025-10-16 11:56:14 -0400

    fix: move constants out to root folder

--- app/constants.ts
+++ app/constants.ts
@@ -1,5 +1,9 @@
+// the endpoint requires image height and width to be a multiple of 8
 export const COMPUTER_IMAGE_MAX_WIDTH = 496;
 export const PHOTO_IMAGE_MAX_WIDTH = 456;
 export const PHOTO_IMAGE_MAX_HEIGHT = 342;
 export const RECEIPT_MAX_WIDTH = 512;
 export const RECURSE_COMPUTER_BORDER = 20;
+
+export const OLD_PRINTER_ENDPOINT = "/receipt/escpos";
+export const NEW_PRINTER_ENDPOINT = "https://receipt.recurse.com/escpos";
